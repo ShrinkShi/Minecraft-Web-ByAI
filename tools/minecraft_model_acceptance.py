b90fefeb86d0222cf336590faa86939cebc20462
Shrink <162001247+ShrinkShi@users.noreply.github.com>
2026-08-16 14:14:49 +0800

    assets: centralize Minecraft model acceptance roots

---
+++ tools/minecraft_model_acceptance.py
@@ -0,0 +1,13 @@
+"""Shared acceptance roots for the first generic Minecraft block-model pipeline."""
+
+MINECRAFT_MODEL_ACCEPTANCE_BLOCKS = (
+    "minecraft:iron_ore",
+    "minecraft:glass",
+    "minecraft:oak_slab",
+    "minecraft:oak_stairs",
+    "minecraft:oak_door",
+    "minecraft:oak_fence",
+    "minecraft:torch",
+    "minecraft:grass_block",
+    "minecraft:crafting_table",
+)
