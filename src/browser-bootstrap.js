db2177b93ab3d8e87e275ef2ea5df6892bb013ca
Shrink <162001247+ShrinkShi@users.noreply.github.com>
2026-08-18 00:53:25 +0800

    feat: install inventory Steve preview

--- src/browser-bootstrap.js
+++ src/browser-bootstrap.js
@@ -1,6 +1,9 @@
 import {installVanillaUiPresentation} from './vanilla-ui-presentation.js';
 import {installWorldSelection} from './world-selection.js';
+import {installInventoryPlayerPreview} from './inventory-player-preview.js';
 import './main.js';
 
 installVanillaUiPresentation();
 installWorldSelection();
+const inventoryPreview=installInventoryPlayerPreview();
+if(globalThis.__minecraftE2E&&inventoryPreview)globalThis.__minecraftE2E.inventoryPlayerPreview=()=>inventoryPreview.snapshot();
