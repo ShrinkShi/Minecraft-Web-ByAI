bb0befa6a144acb85d4cdc5825118fe64f8894a1
Shrink <162001247+ShrinkShi@users.noreply.github.com>
2026-08-21 20:28:51 +0800

    fix: install mining audio runtime bridge

--- src/browser-bootstrap.js
+++ src/browser-bootstrap.js
@@ -1,5 +1,6 @@
 import {installVanillaUiPresentation} from './vanilla-ui-presentation.js';
 import {installVanillaWorkbenchPresentation} from './vanilla-workbench-presentation.js';
+import {installVanillaMiningAudioRuntime} from './vanilla-mining-audio-runtime.js';
 import {installWorldSelection} from './world-selection.js';
 import {installInventoryPlayerPreview} from './inventory-player-preview.js';
 import {installImmersiveGameShell} from './immersive-game-shell.js';
@@ -8,6 +9,7 @@ import './main.js';
 
 installVanillaUiPresentation();
 installVanillaWorkbenchPresentation();
+installVanillaMiningAudioRuntime();
 installWorldSelection();
 const inventoryPreview=installInventoryPlayerPreview();
 installImmersiveGameShell(document.querySelector('#game-canvas'));
