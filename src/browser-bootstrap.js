98558664ae4afadb70913d332470dbaadf3d2b35
Shrink <162001247+ShrinkShi@users.noreply.github.com>
2026-08-21 15:53:52 +0800

    fix: install canonical workbench presentation after vanilla ui

--- src/browser-bootstrap.js
+++ src/browser-bootstrap.js
@@ -1,4 +1,5 @@
 import {installVanillaUiPresentation} from './vanilla-ui-presentation.js';
+import {installVanillaWorkbenchPresentation} from './vanilla-workbench-presentation.js';
 import {installWorldSelection} from './world-selection.js';
 import {installInventoryPlayerPreview} from './inventory-player-preview.js';
 import {installImmersiveGameShell} from './immersive-game-shell.js';
@@ -6,6 +7,7 @@ import './furnace-ui.js';
 import './main.js';
 
 installVanillaUiPresentation();
+installVanillaWorkbenchPresentation();
 installWorldSelection();
 const inventoryPreview=installInventoryPlayerPreview();
 installImmersiveGameShell(document.querySelector('#game-canvas'));
